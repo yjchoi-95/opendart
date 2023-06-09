24d201bca018e26a562418f3e2697c6b83b10b61
yjchoi <65330431+yjchoi-95@users.noreply.github.com>
2023-06-09 12:56:57 +0900

    file upload

--- ipynb/.ipynb_checkpoints/Corporate-Finance-dept1-checkpoint.py
+++ ipynb/.ipynb_checkpoints/Corporate-Finance-dept1-checkpoint.py
@@ -42,7 +42,8 @@ def get_driver():
     #return webdriver.Chrome(options=options, service_log_path='selenium.log')
     #return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     #return webdriver.Chrome("/app/opendart/ipynb/chromedriver", options=options)
-    return webdriver.Chrome(options=options)
+    return webdriver.Chrome("/app/opendart/ipynb/chromedriver.exe", options=options)
+    #return webdriver.Chrome(options=options)
     #return webdriver.Chrome(options=options)
 
 def to_excel(df):
