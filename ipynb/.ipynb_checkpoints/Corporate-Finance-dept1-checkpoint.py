72b63f1420321bea8e3e2b906325e973c05c84bb
yjchoi <65330431+yjchoi-95@users.noreply.github.com>
2023-06-12 09:52:26 +0900

    change_path

--- ipynb/.ipynb_checkpoints/Corporate-Finance-dept1-checkpoint.py
+++ ipynb/.ipynb_checkpoints/Corporate-Finance-dept1-checkpoint.py
@@ -62,8 +62,9 @@ def get_driver():
     #ChromeDriverManager().install()
     #Service(ChromeDriverManager().install())
     #os.chmod('/app/opendart/ipynb/chromedriver', 755)
-    os.chmod('/app/opendart/ipynb/', 755)
-    return webdriver.Chrome(executable_path = "/app/opendart/ipynb/chromedriver", options = options)
+    os.chmod('./app/opendart/ipynb/chromedriver', 755)
+    #return webdriver.Chrome(executable_path = "./app/opendart/ipynb/chromedriver", service = Service(ChromeDriverManager().install()), options = options)
+    return webdriver.Chrome(executable_path = "./app/opendart/ipynb/chromedriver", options = options)
 
     #return webdriver.Chrome("/app/opendart/ipynb/chromedriver.exe", options=options)
     #return webdriver.Chrome(executable_path = "/app/opendart/ipynb/chromedriver", options = options)
