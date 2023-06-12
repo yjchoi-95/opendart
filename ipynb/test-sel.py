66d7136ce4f7a98b4a54001620aaa4cb941d34d3
yjchoi <65330431+yjchoi-95@users.noreply.github.com>
2023-06-12 20:58:31 +0900

    change chrome

--- ipynb/test-sel.py
+++ ipynb/test-sel.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from webdriver_manager.firefox import GeckoDriverManager
+#from selenium.webdriver import FirefoxOptions
+#from selenium.webdriver.firefox.service import Service
+#from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+#from webdriver_manager.firefox import GeckoDriverManager
+
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
 import streamlit as st
 import os, sys
 
@@ -13,8 +19,9 @@ import subprocess
 @st.cache_resource
 def installff():
     #os.system('/home/appuser/venv/bin/python -m pip install --upgrade pip')
-    os.system('cd . add-apt-repository ppa:ubuntu-mozilla-security/ppa')
-    os.system('sbase install geckodriver')
+    #os.system('cd . add-apt-repository ppa:ubuntu-mozilla-security/ppa')
+    #os.system('sbase install geckodriver')
+    os.system('sbase install chromedriver')
     #os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
     #os.system('export PATH=$PATH:/home/appuser/venv/bin/geckodriver')
     #os.system('export PATH=$PATH:/home/appuser/venv/bin/firefox')
@@ -24,22 +31,24 @@ def installff():
 _ = installff()
 st.write("finish-install")
 
-opts = FirefoxOptions()
-opts.add_argument("--headless")
+#opts = FirefoxOptions()
+#opts.add_argument("--headless")
 #firefox_binary = FirefoxBinary('/home/appuser/venv/bin/firefox.exe')
 #st.write("set-binary")
 
 #opts.binary = firefox_binary
 #opts.binary_location = '/home/appuser/venv/bin/geckodriver'
 #opts.binary_location = '/home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver.exe'
-opts.binary_location = '/home/appuser/venv/bin/firefox.exe'
+#opts.binary_location = '/home/appuser/venv/bin/firefox.exe'
 
 #service = Service(GeckoDriverManager().install())
 st.write("set-service")
 #browser = webdriver.Firefox(service = service, options=opts)
 st.write("run-browser")
-browser = webdriver.Firefox(options=opts)
+#browser = webdriver.Firefox(options=opts)
+options = Options()
+options.add_argument("--headless")
+browser = webdriver.Chrome(Service(ChromeDriverManager.install()), options = options)
 
-
-browser.get('http://naver.com')
+#browser.get('http://naver.com')
 st.write(browser.page_source)
