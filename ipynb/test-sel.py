437d069b5aefd5a002a2751fa6ad5f70b2600923
yjchoi <65330431+yjchoi-95@users.noreply.github.com>
2023-06-12 20:14:23 +0900

    change

--- ipynb/test-sel.py
+++ ipynb/test-sel.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
 import streamlit as st
 import os, sys
 
@@ -7,13 +9,16 @@ def installff():
     os.system('sbase install geckodriver')
     os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
 
+    
 _ = installff()
 
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
 opts = FirefoxOptions()
 opts.add_argument("--headless")
-browser = webdriver.Firefox(options=opts)
+service = Service(GeckoDriverManager().install())
+browser = webdriver.Firefox(service = service, options=opts)
+
 
 browser.get('http://naver.com')
 st.write(browser.page_source)
