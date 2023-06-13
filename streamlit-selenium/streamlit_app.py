b207acb9980254adc6fa55be898d77f77608cfa4
yjchoi <65330431+yjchoi-95@users.noreply.github.com>
2023-06-13 10:48:34 +0900

    c

--- streamlit-selenium/streamlit_app.py
+++ streamlit-selenium/streamlit_app.py
@@ -66,10 +66,11 @@ def run_selenium():
 #    os.system('find / -name Mozila')
 #    os.system('find / -name geckodriver')
     
+    service = Service(executable_path = '/app/opendart/streamlit-selenium/chromedriver')
     
     #with webdriver.Chrome(options=options, service_log_path='selenium.log') as driver:
-    #with webdriver.Chrome(service = service, options=options) as driver:
-    with webdriver.Chrome(options=options) as driver:
+    with webdriver.Chrome(service = service, options=options) as driver:
+    #with webdriver.Chrome(options=options) as driver:
         url = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
         driver.get(url)
         xpath = '//*[@class="ui-mainview-block eventpath-wrapper"]'
