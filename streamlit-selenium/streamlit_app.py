4677f2eb03ce574f70491824482610dc40acbc01
yjchoi <65330431+yjchoi-95@users.noreply.github.com>
2023-06-13 09:55:58 +0900

    c

--- streamlit-selenium/streamlit_app.py
+++ streamlit-selenium/streamlit_app.py
@@ -17,7 +17,7 @@ options.add_argument("--window-size=1920x1080")
 options.add_argument("--disable-features=VizDisplayCompositor")
 #options.binary_location('/home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/chromedriver')
 
-service = Service(executable_path = '/home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/chromedriver')
+#service = Service(executable_path = '/home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/chromedriver')
 
 def delete_selenium_log():
     if os.path.exists('selenium.log'):
@@ -32,13 +32,14 @@ def show_selenium_log():
 def run_selenium():
     name = str()
     os.system('sbase get chromedriver 114.0.5735.90')
+    os.system('pwd')
     os.system('which chromedriver')
     os.system('which chrome.exe')
     #os.system('locate chromedriver')
     #os.system('locate google-chrome')
     #with webdriver.Chrome(options=options, service_log_path='selenium.log') as driver:
-    with webdriver.Chrome(service = service, options=options) as driver:
-    #with webdriver.Chrome(options=options) as driver:
+    #with webdriver.Chrome(service = service, options=options) as driver:
+    with webdriver.Chrome(options=options) as driver:
         url = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
         driver.get(url)
         xpath = '//*[@class="ui-mainview-block eventpath-wrapper"]'
